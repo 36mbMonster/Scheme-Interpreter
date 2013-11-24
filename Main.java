2e7a2e07933e35c9093b4fdb02855ee45c36f3c0
Derrick Liang <liangderrick@yahoo.com>
2013-11-23 18:32:20 -0800

    Some scheme code (in java)
    added into main

--- Main.java
+++ Main.java
@@ -6,14 +6,52 @@
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  *
  * @author Steven
  */
+
 public class Main {
 
-    /**
+	public String car(ArrayList<String> a)
+	{
+		if (a.size() != 0)
+			return a.get(0);
+		else
+			return null;
+
+	}
+	
+	public ArrayList<String> cdr(ArrayList<String> a)
+	{
+		ArrayList<String> b = new ArrayList<String>();
+		if (a.size() == 0)
+			return b;
+		else
+		{
+			for (int i = 1; i < a.size(); i++)
+				b.add(a.get(i));
+			return b;
+		}
+	}
+	
+	public ArrayList<String> cons(String s, ArrayList<String> a)
+	{
+		a.add(0, s);
+		return a;
+	}
+    
+	public boolean is_null(ArrayList<String> a)
+	{
+		if (a.size() == 0)
+			return true;
+		else
+			return false;
+	}
+	
+	/**
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException {
